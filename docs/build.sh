8452384b4d97c5df5ce034c4d35f54d411e0ae3f
Satria Aji Putra <satriamaxt@gmail.com>
2026-03-09 05:35:44 +0700

    fix: fix zola download in build.sh
    Remove -J flag to avoid filename issues, add -f to fail on HTTP errors,
    use explicit -o for output filename, and add -z flag to tar.
    
    Signed-off-by: Satria Aji Putra <satriamaxt@gmail.com>

--- docs/build.sh
+++ docs/build.sh
@@ -4,8 +4,9 @@ set -euo pipefail
 main() {
     ZOLA_VERSION=0.22.1
 
-    curl -sLJO "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
-    tar -xf zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz
+    curl -sfL "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz" \
+        -o "zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+    tar -xzf zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz
 
     git submodule update --init --recursive
 
